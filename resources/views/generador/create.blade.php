660caf1da2f59190609795b8be5ec25cdead9f39
Roelote <jroelx@gmail.com>
2021-05-26 10:51:43 -0500

    se agrego nuevo select dinamico

--- resources/views/generador/create.blade.php
+++ resources/views/generador/create.blade.php
@@ -72,15 +72,14 @@
                 </div>
             </div>
             <div class="form-row">
-                <table class="table table-bordered" id="dynamicTable">
+                <table class="table table-bordered" id="dynamic_field">
 
                     <tr>
-                        <th>Nombre Hotel</th>
-                        <th>Accion</th>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="addmore[0][name]" placeholder="Enter your Name" class="form-control" /></td>
-                        <td><button type="button" name="add" id="add" class="btn btn-success">Agregar Hotel</button></td>
+
+                        <td><input type="text" name="name[]" placeholder="Enter your Name" class="form-control name_list" /></td>
+
+                        <td><button type="button" name="add" id="add" class="btn btn-success">Add More</button></td>
+
                     </tr>
 
                 </table>
@@ -140,20 +139,106 @@
 </script>
 
 <script type="text/javascript">
-    var i = 0;
 
-    $("#add").click(function(){
+    $(document).ready(function(){
 
-        ++i;
+      var postURL = "<?php echo url('addmore'); ?>";
 
-        $("#dynamicTable").append('<tr><td><input type="text" name="addmore['+i+'][name]" placeholder="Enter your Name" class="form-control" /></td><td><button type="button" class="btn btn-danger remove-tr">Borrar</button></td></tr>');
+      var i=1;
+
+
+      $('#add').click(function(){
+
+           i++;
+
+           $('#dynamic_field').append('<tr id="row'+i+'" class="dynamic-added"><td><input type="text" name="name[]" placeholder="Enter your Name" class="form-control name_list" /></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
+
+      });
+
+
+      $(document).on('click', '.btn_remove', function(){
+
+           var button_id = $(this).attr("id");
+
+           $('#row'+button_id+'').remove();
+
+      });
+
+
+      $.ajaxSetup({
+
+          headers: {
+
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+
+          }
+
+      });
+
+
+      $('#submit').click(function(){
+
+           $.ajax({
+
+                url:postURL,
+
+                method:"POST",
+
+                data:$('#add_name').serialize(),
+
+                type:'json',
+
+                success:function(data)
+
+                {
+
+                    if(data.error){
+
+                        printErrorMsg(data.error);
+
+                    }else{
+
+                        i=1;
+
+                        $('.dynamic-added').remove();
+
+                        $('#add_name')[0].reset();
+
+                        $(".print-success-msg").find("ul").html('');
+
+                        $(".print-success-msg").css('display','block');
+
+                        $(".print-error-msg").css('display','none');
+
+                        $(".print-success-msg").find("ul").append('<li>Record Inserted Successfully.</li>');
+
+                    }
+
+                }
+
+           });
+
+      });
+
+
+      function printErrorMsg (msg) {
+
+         $(".print-error-msg").find("ul").html('');
+
+         $(".print-error-msg").css('display','block');
+
+         $(".print-success-msg").css('display','none');
+
+         $.each( msg, function( key, value ) {
+
+            $(".print-error-msg").find("ul").append('<li>'+value+'</li>');
+
+         });
+
+      }
+
     });
 
-    $(document).on('click', '.remove-tr', function(){
-
-         $(this).parents('tr').remove();
-
-    });
 </script>
 
 
